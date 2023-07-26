176fdf65c6b9b92f540b5301f6886c8e1fce4380
Uttam Bhavani <uttambhavani007@gmail.com>
2023-07-26 19:59:08 +0530

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,9 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
-  cookieKey: '123123123',
+    "mongodb://uttam:uttam@ac-hnl4syc-shard-00-00.ztudreh.mongodb.net:27017,ac-hnl4syc-shard-00-01.ztudreh.mongodb.net:27017,ac-hnl4syc-shard-00-02.ztudreh.mongodb.net:27017/?ssl=true&replicaSet=atlas-k9oebd-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
